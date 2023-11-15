2972ac492771a838601ecb97cb17f949c77458ca
Morty052 <anomaly619052@gmail.com>
2023-11-15 02:42:50 -0800

    k

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
-import express from "https://esm.sh/express";
-import cors from "https://esm.sh/cors";
+import express from "npm:express";
+import cors from "npm:cors";
 // import bodyParser from "https://esm.sh/body-parser";
 import send from "./send.ts";
 
